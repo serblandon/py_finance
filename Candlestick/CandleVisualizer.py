2cecfcc9abca3d07097c57de2cb5857b077ebbf8
uif35305 <serban-ioan.crasovan@continental-corporation.com>
2022-09-29 14:08:57 +0300

    Simple

--- Candlestick/CandleVisualizer.py
+++ Candlestick/CandleVisualizer.py
@@ -1,20 +1,9 @@
-import datetime as dt
-import pandas_datareader as web
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import mplfinance as mpf
+import yfinance as yf
+import pandas as pd
+import mplfinance as mpl
 
-# Time frame
+df = yf.Ticker("META").history(period="max")
 
-start_date = dt.datetime(2022, 9, 1)
-finish_date = dt.datetime.now()
+df = df.loc["2021-09-30":]
 
-# Data
-data = web.DataReader("META", "yahoo", start_date, finish_date)
-
-# insert index column first
-#data.reset_index(inplace=True)
-
-mpf.plot(data, type="candle", volume=True, show_nontrading=True)
-
-print(data)
+mpl.plot(df, type="candle", volume=True)
